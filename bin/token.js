2a0b383188b7cdf7e8b6450efee9eb488b5df30e
zhujiajia <772978409@qq.com>
2018-03-01 13:11:28 +0800

    获取并保存jsapi_ticket

--- bin/token.js
+++ bin/token.js
@@ -2,6 +2,7 @@
  * Created by zjj on 2017/11/11.
  * 执行程序			node ./token.js
  * 程序执行后每隔60分钟获取access_token并存入redis（redis失效时间120分钟）
+ * 显示access_token和jsapi_ticket   https://weiquaninfo.cn/wxPublicAccount/getAccessToken
  */
 
 //
@@ -13,6 +14,7 @@ const appid = 'wxaed97ec85f7517ba';
 const screct = 'fecd688110e195c4ea0e9f3d97797d44';
 const url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + screct;
 const key = "token";
+const keyTicket = "jsapi_ticket";
 const time = 7200; //2小时
 const intervalTime = 3600000; //1小时
 // const intervalTime = 10000; //10秒，测试用
@@ -86,6 +88,49 @@ function getAccessToken() {
 			logger.info("access_token：" + access_token);
 			redisClient.set(key, access_token);
 			redisClient.expire(key, time); //过期时间设置
+			// 获取jsapi-ticket
+			getJsapi_ticket(access_token);
+
+		});
+	}).on('error', function(e) {
+		console.error(e.message);
+	})
+}
+
+
+// 获取jsapi-ticket
+function getJsapi_ticket(access_token) {
+	var token = access_token.substring(1, access_token.length - 1);
+	var url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + token + "&type=jsapi";
+	// logger.info("url_ticket：" + url);
+	https.get(url, function(res) {
+		const statusCode = res.statusCode;
+		const contentType = res.headers['content-type'];
+		var error = null;
+		if (statusCode !== 200) {
+			error = "请求jsapi-ticket失败。状态码: " + statusCode;
+		} else if (!/^application\/json/.test(contentType)) {
+			error = "无效的 content-type";
+		}
+		if (error) {
+			logger.error(error);
+			// 消耗响应数据以释放内存
+			res.resume();
+			return;
+		}
+
+		// 监听事件
+		var rawData = '';
+		res.setEncoding('utf8');
+		res.on('data', function(chunk) {
+			rawData += chunk;
+		});
+		res.on('end', function() {
+			var data = JSON.parse(rawData);
+			var jsapi_ticket = JSON.stringify(data.ticket);
+			logger.info("jsapi_ticket：" + jsapi_ticket);
+			redisClient.set(keyTicket, jsapi_ticket);
+			redisClient.expire(keyTicket, time); //过期时间设置
 		});
 	}).on('error', function(e) {
 		console.error(e.message);
