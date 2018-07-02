617a59fc8768fee7aba1f7db8127b19b6d1d4bf9
zhujiajia <zhujiajia2003@163.com>
2018-07-02 16:42:03 +0800

    add api_ticket

--- bin/token.js
+++ bin/token.js
@@ -15,6 +15,7 @@ const screct = 'fecd688110e195c4ea0e9f3d97797d44';
 const url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + screct;
 const key = "token";
 const keyTicket = "jsapi_ticket";
+const keyTicket2 = "api_ticket";
 const time = 7200; //2小时
 const intervalTime = 3600000; //1小时
 // const intervalTime = 10000; //10秒，测试用
@@ -90,6 +91,8 @@ function getAccessToken() {
             redisClient.expire(key, time); //过期时间设置
             // 获取jsapi-ticket
             getJsapi_ticket(access_token);
+            // 获取api-ticket（微信卡券）
+            get_api_ticket(access_token);
 
         });
     }).on('error', function(e) {
@@ -131,6 +134,45 @@ function getJsapi_ticket(access_token) {
             logger.info("jsapi_ticket：" + jsapi_ticket);
             redisClient.set(keyTicket, jsapi_ticket);
             redisClient.expire(keyTicket, time); //过期时间设置
+        });
+    }).on('error', function(e) {
+        console.error(e.message);
+    })
+}
+
+// 获取api-ticket
+function get_api_ticket(access_token) {
+    var token = access_token.substring(1, access_token.length - 1);
+    var url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + token + "&type=wx_card";
+    // logger.info("url_ticket：" + url);
+    https.get(url, function(res) {
+        const statusCode = res.statusCode;
+        const contentType = res.headers['content-type'];
+        var error = null;
+        if (statusCode !== 200) {
+            error = "请求jsapi-ticket失败。状态码: " + statusCode;
+        } else if (!/^application\/json/.test(contentType)) {
+            error = "无效的 content-type";
+        }
+        if (error) {
+            logger.error(error);
+            // 消耗响应数据以释放内存
+            res.resume();
+            return;
+        }
+
+        // 监听事件
+        var rawData = '';
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            rawData += chunk;
+        });
+        res.on('end', function() {
+            var data = JSON.parse(rawData);
+            var api_ticket = JSON.stringify(data.ticket);
+            logger.info("api_ticket：" + api_ticket);
+            redisClient.set(keyTicket2, api_ticket);
+            redisClient.expire(keyTicket2, time); //过期时间设置
         });
     }).on('error', function(e) {
         console.error(e.message);
